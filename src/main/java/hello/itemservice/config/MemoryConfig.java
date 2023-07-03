8efaad2e501b34f5ef62bb839d01024397b22d7d
baekinsoo <baikinsoo95@gmail.com>
2023-07-03 18:07:32 +0900

    Initial commit

---
+++ src/main/java/hello/itemservice/config/MemoryConfig.java
@@ -0,0 +1,25 @@
+package hello.itemservice.config;
+
+import hello.itemservice.repository.ItemRepository;
+import hello.itemservice.repository.memory.MemoryItemRepository;
+import hello.itemservice.service.ItemService;
+import hello.itemservice.service.ItemServiceV1;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+public class MemoryConfig {
+
+    @Bean
+    public ItemService itemService() {
+        return new ItemServiceV1(itemRepository());
+    }
+
+    @Bean
+    public ItemRepository itemRepository()  {
+        return new MemoryItemRepository();
+    }
+
+}
